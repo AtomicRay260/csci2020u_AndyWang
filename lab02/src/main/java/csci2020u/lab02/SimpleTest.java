4393c2c86d753ee46825d6578a9615fe23884e26
AtomicRay260 <wei.wang8@ontariotechu.net>
2021-02-04 23:51:24 -0500

    Third attemp at build part 1

--- lab02/src/main/java/csci2020u/lab02/SimpleTest.java
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -1,6 +1,6 @@
-package lab02;
+package csci2020u.lab02;
 
-public class SimpleTest{
+public class SimpleTest {
     public static void main(String args[]){
         System.out.println("hello world");
     }
